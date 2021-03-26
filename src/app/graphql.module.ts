f5a30e85cc6ff3333bdf11ff21d6ff9c9e75473e
brench12 <renchdev@gmail.com>
2021-03-25 20:12:55 -0400

    Update token.

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -13,7 +13,7 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   }));
 
   const auth = setContext((operation, context) => {
-    const token = '4b1dcf7733e0d7dc0bb649ba6683440a244f5a19'; // set this to a valid GitHub personal token with appropriate rights for searching users
+    const token = 'f1aad0fefed787ebb194c355b146441b3bc938a5'; // set this to a valid GitHub personal token with appropriate rights for searching users
 
     if (token === null) {
       return {};
